95493aa1d6971ab720afc63b7ad7dc10a3830a89
Rikki-Wallis <rikkiwallis2004@gmail.com>
2026-01-20 19:11:25 +1100

    Add register new account ui and forms

--- src/app/auth/register/register.ts
+++ src/app/auth/register/register.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AbstractControl } from '@angular/forms';
+import { ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,
+            CommonModule,
+            RouterModule,
+  ],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
@@ -15,10 +22,26 @@ export class Register {
   constructor(private fb: FormBuilder) {
     this.registerForm = this.fb.group({
         email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required, Validators.required]],
-    });
-  }
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required]
+    }, 
+    { validators: this.passwordsMatchValidator});
+  } 
 
+  // Confirm Password Validator
+    passwordsMatchValidator(form: AbstractControl): ValidationErrors | null {
+      
+      const password = form.get('password')?.value;
+      const confirmPassword = form.get('confirmPassword')?.value;
+
+      if (password && confirmPassword && password !== confirmPassword) {
+        return { passwordsMismatch: true };
+      }
+
+      return null;
+    }
+
+    // Submit Function
   submit() {
     if (this.registerForm.invalid) return;
     
